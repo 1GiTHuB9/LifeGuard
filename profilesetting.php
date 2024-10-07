5fe2c98a6bf45f287183bbc0c8ff24cefec9359b
2101005 <116612779+2101005@users.noreply.github.com>
2024-10-07 12:16:11 +0900

    uemura

--- profilesetting.php
+++ profilesetting.php
@@ -5,34 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>プロフィール画面</title>
     <link rel="stylesheet" href="./css/style1.css">
-    <style>
-        .icon {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%; /* 丸くする */
-    object-fit: cover; /* 画像を要素にフィットさせる */
-    border: 2px solid #000000;
-  }
-    </style>
+
+
 </head>
 <body>
-    <!-- <div class="fullscreen-image"> -->
+    <div class="fullscreen-image">
 
-        <!-- <img src="img/haikei4.png" alt="Full Screen Image"> -->
-        
+        <img src="img/haikei4.png" alt="Full Screen Image">
+    </div>
+    <div class="container1">
         <a href="#" class="back-button" onclick="goBack()">←戻る</a>
 
         <form action="profilesetting.php" class="setting-form">
-            <img src="img/haikei4.png" alt="アイコン" class="icon">
+            <img src="img/haikei4.png" class="userimage">
 
-            <h5>ここにプロフィールを表示</h5>
 
+            <div class="username">ユーザー名</div><br><br>
+            <div class="user-profile">ユーザープロフィール</div>
+            <textarea  class="userprofile">デフォルトの値
+            </textarea><br>
+
+
+
+            <button type="submit">更新</button>
         </form>
+    </div>
+        
     
 
 
       
-    <!-- </div> -->
+    
     <script>
         function goBack() {
             history.back();
