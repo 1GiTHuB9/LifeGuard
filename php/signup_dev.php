25fbcec89ba00b78a66efb064d56337fdbc41b30
ASO2201117 <2201117@s.asojuku.ac.jp>
2024-11-11 12:48:35 +0900

    tenaosi

--- php/signup_dev.php
+++ php/signup_dev.php
@@ -6,12 +6,13 @@ try {
     $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     // SQL文を準備
-    $sql = "INSERT INTO users(mailaddress, password) VALUES (?, ?)";
+    $sql = "INSERT INTO users(mailaddress, password,user_name) VALUES (?, ?,?)";
     $stmt = $pdo->prepare($sql); 
 
     // 値をバインド
     $stmt->bindValue(1, $_POST['email'], PDO::PARAM_STR);
     $stmt->bindValue(2, $hashedPassword, PDO::PARAM_STR);
+    $stmt->bindValue(3, $_POST['name'], PDO::PARAM_STR);
 
     // クエリ実行
     $stmt->execute();
